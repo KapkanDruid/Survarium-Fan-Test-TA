9b59eed1b052cce9b2bccd5b216ded3bf6bf0549
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-01-30 08:24:06 -0800

    refactor: данные игрока перенесены в PlayerConfig

---
+++ Assets/Scripts/Content/PlayerLogic/PlayerConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Content.PlayerLogic
+{
+    [CreateAssetMenu(menuName = "Survarium/Configs/" + nameof(PlayerConfig))]
+    public class PlayerConfig : ScriptableObject
+    {
+        [SerializeField] private float _moveSpeedOnGround;
+        [SerializeField] private float _moveSpeedOnFall;
+        [SerializeField] private float _jumpTime;
+        [SerializeField] private float _jumpHeight;
+
+        public float MoveSpeedOnGround => _moveSpeedOnGround;
+        public float JumpTime => _jumpTime;
+        public float JumpHeight => _jumpHeight;
+        public float MoveSpeedOnFall => _moveSpeedOnFall;
+    }
+}
--- Assets/Scripts/Content/PlayerLogic/PlayerData.cs
+++ Assets/Scripts/Content/PlayerLogic/PlayerData.cs
@@ -5,19 +5,16 @@ namespace Assets.Scripts.Content.PlayerLogic
 {
     public class PlayerData : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeedOnGround;
-        [SerializeField] private float _moveSpeedOnFall;
-        [SerializeField] private float _jumpTime;
-        [SerializeField] private float _jumpHeight;
+        [SerializeField] private PlayerConfig _playerConfig;
 
         [SerializeField] private Transform _playerTransform;
         [SerializeField] private GameObject _characterObject;
         [SerializeField] private CinemachinePanTilt _cinemachinePanTilt;
 
-        public float MoveSpeedOnGround => _moveSpeedOnGround;
-        public float JumpTime => _jumpTime;
-        public float JumpHeight => _jumpHeight;
-        public float MoveSpeedOnFall => _moveSpeedOnFall;
+        public float MoveSpeedOnGround => _playerConfig.MoveSpeedOnGround;
+        public float JumpTime => _playerConfig.JumpTime;
+        public float JumpHeight => _playerConfig.JumpHeight;
+        public float MoveSpeedOnFall => _playerConfig.MoveSpeedOnFall;
 
         public Transform PlayerTransform => _playerTransform;
         public GameObject CharacterObject => _characterObject;
